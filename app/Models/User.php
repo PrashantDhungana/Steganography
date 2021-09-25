4a476b37c83a2d6aba334490fe2fff45a2631e5c
RabiNeupane-12 <rabi.neupane.rn@gmail.com>
2021-09-25 22:05:02 +0545

    ;o

--- app/Models/User.php
+++ app/Models/User.php
@@ -22,7 +22,7 @@ class User extends Authenticatable
         'email',
         'password',
     ];
-
+    
     /**
      * The attributes that should be hidden for serialization.
      *
@@ -41,4 +41,14 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+    public function gallery()
+    {
+        return $this->hasMany(\App\gallery::class);
+    }
+    public function email()
+    {
+        return $this->hasOne(\App\email::class);
+    }
+
+
 }
