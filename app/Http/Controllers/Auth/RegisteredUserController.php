d396b886f06e20c9efe41bc7d60b3c7ccafbc779
PrashantDhungana <sharma.prashant2000@gmail.com>
2021-12-13 19:30:19 +0545

    [ADDED] Steg function

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -33,18 +33,29 @@ class RegisteredUserController extends Controller
      */
     public function store(Request $request)
     {
-        $attributes=$request->validate([
+        $request->validate([
             'name'=> ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255'],
-            'password' => ['required', 'confirmed'],
+            'password' => ['required', 'confirmed','min:7','max:255'],
             'passimg' => ['required', 'image']
         ]);
 
-        dd('here');
+        $hashedPassword = bcrypt($request->password);
+        $file = $request->file('passimg') ;
+        $this->steganize($file,$hashedPassword);
+        dd($this->desteganize('secret.png'));
+    //     if($file = $request->hasFile('image')) {
+             
+    //         $fileName = $file->getClientOriginalName() ;
+    //         $destinationPath = public_path().'/images' ;
+    //         $file->move($destinationPath,$fileName);
+    //         return redirect('/uploadfile');
+    // }
+
         $user = User::create([
             'username' => $request->name,
             'email' => $request->email,
-            'passimage' => Hash::make($request->password),
+            'password' => $hashedPassword,
         ]);
     
 
@@ -54,5 +65,121 @@ class RegisteredUserController extends Controller
 
         return redirect(RouteServiceProvider::HOME);
        
+    }
+    
+    public function steganize($file, $message) 
+    {
+        // Encode the message into a binary string.
+        $binaryMessage = '';
+        for ($i = 0; $i < mb_strlen($message); ++$i) {
+          $character = ord($message[$i]);
+          $binaryMessage .= str_pad(decbin($character), 8, '0', STR_PAD_LEFT);
+        }
+      
+        // Inject the 'end of text' character into the string.
+        $binaryMessage .= '00000011';
+      
+        // Load the image into memory.
+        $img = imagecreatefromjpeg($file);
+      
+        // Get image dimensions.
+        $width = imagesx($img);
+        $height = imagesy($img);
+      
+        $messagePosition = 0;
+      
+        for ($y = 0; $y < $height; $y++) {
+          for ($x = 0; $x < $width; $x++) {
+      
+            if (!isset($binaryMessage[$messagePosition])) {
+              // No need to keep processing beyond the end of the message.
+              break 2;
+            }
+      
+            // Extract the colour.
+            $rgb = imagecolorat($img, $x, $y);
+            $colors = imagecolorsforindex($img, $rgb);
+      
+            $red = $colors['red'];
+            $green = $colors['green'];
+            $blue = $colors['blue'];
+            $alpha = $colors['alpha'];
+      
+            // Convert the blue to binary.
+            $binaryBlue = str_pad(decbin($blue), 8, '0', STR_PAD_LEFT);
+      
+            // Replace the final bit of the blue colour with our message.
+            $binaryBlue[strlen($binaryBlue) - 1] = $binaryMessage[$messagePosition];
+            $newBlue = bindec($binaryBlue);
+      
+            // Inject that new colour back into the image.
+            $newColor = imagecolorallocatealpha($img, $red, $green, $newBlue, $alpha);
+            imagesetpixel($img, $x, $y, $newColor);
+      
+            // Advance message position.
+            $messagePosition++;
+          }
+        }
+      
+        // Save the image to a file.
+        $newImage = 'secret.png';
+        $op = imagepng($img, $newImage, 9);
+        // Destroy the image handler.
+        imagedestroy($img);
+    }
+
+    public function desteganize($file) {
+        // Read the file into memory.
+        $img = imagecreatefrompng($file);
+      
+        // Read the message dimensions.
+        $width = imagesx($img);
+        $height = imagesy($img);
+      
+        // Set the message.
+        $binaryMessage = '';
+      
+        // Initialise message buffer.
+        $binaryMessageCharacterParts = [];
+      
+        for ($y = 0; $y < $height; $y++) {
+          for ($x = 0; $x < $width; $x++) {
+      
+            // Extract the colour.
+            $rgb = imagecolorat($img, $x, $y);
+            $colors = imagecolorsforindex($img, $rgb);
+      
+            $blue = $colors['blue'];
+      
+            // Convert the blue to binary.
+            $binaryBlue = decbin($blue);
+      
+            // Extract the least significant bit into out message buffer..
+            $binaryMessageCharacterParts[] = $binaryBlue[strlen($binaryBlue) - 1];
+      
+            if (count($binaryMessageCharacterParts) == 8) {
+              // If we have 8 parts to the message buffer we can update the message string.
+              $binaryCharacter = implode('', $binaryMessageCharacterParts);
+              $binaryMessageCharacterParts = [];
+              if ($binaryCharacter == '00000011') {
+                // If the 'end of text' character is found then stop looking for the message.
+                break 2;
+              }
+              else {
+                // Append the character we found into the message.
+                $binaryMessage .= $binaryCharacter;
+              }
+            }
+          }
+        }
+      
+        // Convert the binary message we have found into text.
+        $message = '';
+        for ($i = 0; $i < strlen($binaryMessage); $i += 8) {
+          $character = mb_substr($binaryMessage, $i, 8);
+          $message .= chr(bindec($character));
+        }
+      
+        return $message;
     }
 }
