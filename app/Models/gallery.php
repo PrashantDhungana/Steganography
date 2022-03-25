ed4f96381d3d6c6ede9cbdad8307b5f617f14ba5
Karishma Karki <karkikarishma143@gmail.com>
2022-03-25 21:16:16 +0545

    [add] gallery added in dashboard

--- app/Models/gallery.php
+++ app/Models/gallery.php
@@ -5,10 +5,14 @@ namespace App\Models;
 use App\Http\Traits\EncodeDecodeTrait;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
 
 class Gallery extends Model
 {
     use HasFactory,EncodeDecodeTrait;
+    use SoftDeletes;
+    
     
     public function user()
     {
