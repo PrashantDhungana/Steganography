90d47412974c84c54f254edde41bc4bd2ec372a8
PrashantDhungana <sharma.prashant2000@gmail.com>
2022-04-01 10:12:23 +0545

    [ADD] Added download view

--- app/Http/Controllers/API/UploadController.php
+++ app/Http/Controllers/API/UploadController.php
@@ -15,21 +15,6 @@ class UploadController extends Controller
 {
     use EncodeDecodeTrait;
     public function index(Request $request){
-        
-        // // split the string on commas
-        // $data = explode( ',', $request->image );
-
-        // // Obtain the original content (usually binary data)
-        //     $bin = base64_decode($data[1]);
-
-        // // Load GD resource from binary data
-        // $image = imageCreateFromString($bin);
-        
-        // $img_file = 'apiImages/'.uniqid('api_').'.png';
-        // imagepng($image, $img_file, 9);
-
-        // $imageInfo = $this->steganize($image,$request->encode_text,true);
-        // dd($imageInfo);
 
 
         $base64File = $request->input('image');
--- resources/views/gallery.blade.php
+++ resources/views/gallery.blade.php
@@ -10,6 +10,31 @@
 
                 <div class="card-deck mt-3">
                     @foreach ($gallery as $gall)
+                    <!-- Button trigger modal -->
+<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+  Launch demo modal
+</button>
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
                         <div class="col-3 mt-5">
                             <div class="card position-relative" style="margin-left: 0px; margin-right:0px; height:333px;">
                                 <img class="card-img-top" src="images/{{ $gall->image }}" alt="Card image cap">
